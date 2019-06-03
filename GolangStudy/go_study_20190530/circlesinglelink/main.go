e51c3b9be9774fc28f0fca85427f69afbb7f9ae6
ksheng <294851575@qq.com>
2019-06-03 23:58:40 +0800

    1.20190603打卡

--- GolangStudy/go_study_20190530/circlesinglelink/main.go
+++ GolangStudy/go_study_20190530/circlesinglelink/main.go
@@ -33,7 +33,61 @@ func InsertCatNode(head *CatNode, newCatNode *CatNode) {
 }
 
 //删除一只猫
-func DelCatNode(head *CatNode, id int) {
+func DelCatNode(head *CatNode, id int) *CatNode {
+
+	temp := head
+	helper := head
+	//空链表
+	if temp.next == nil {
+		fmt.Println("这是一个空的环形链表，无法删除")
+		return head
+	}
+
+	//如果只有一个结点
+	if temp.next == head { //只有一个结点
+		temp.next = nil
+		return head
+	}
+
+	//将helper定位到环形列表最后
+	for {
+		if helper.next == head {
+			break
+		}
+		helper = helper.next
+	}
+
+	//如果有两个以上的结点
+	flag := true
+	for {
+		if temp.next == head {
+			//如果已经比较到最后一个，且最后一个还没有比较
+			break
+		}
+		if temp.no == id {
+			if temp == head { //说明删除的的head
+				head = head.next
+			}
+			//恭喜找到,我们也可以直接删除
+			helper.next = temp.next
+			flag = false
+			break
+		}
+		temp = temp.next     //移动【比较】
+		helper = helper.next //移动 【一旦找到要删除的结点 helper】
+	}
+
+	//这里还要比较一次
+	if flag { //如果flag为真 我们上面没有删除
+		if temp.no == id {
+			helper.next = temp.next
+			fmt.Println("cat =%d\n",id)
+		}else{
+			fmt.Println("对不起没有找到")
+		}
+	}
+
+	return head
 
 }
 
@@ -75,6 +129,8 @@ func main() {
 	InsertCatNode(head, cat1)
 	InsertCatNode(head, cat2)
 	InsertCatNode(head, cat3)
+	head=DelCatNode(head,2)
+	head=DelCatNode(head,20)
 	ListCircleLink(head)
 
 }
