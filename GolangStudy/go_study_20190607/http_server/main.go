67e9fcf8297df8142e8da8a860738495c74924b6
ksheng <294851575@qq.com>
2019-06-12 23:33:12 +0800

    20190612打卡

--- GolangStudy/go_study_20190607/http_server/main.go
+++ GolangStudy/go_study_20190607/http_server/main.go
@@ -2,19 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
-
-
-func main() {
-	http.HandleFunc("/",Hello)
-	http.HandleFunc("/user/login",Login)
-	err:=http.ListenAndServe("0.0.0.0:8880",nil)
-	if err != nil {
-		fmt.Println("http_server listen failed,err=",err)
-	}
-
-}
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("handle login")
@@ -23,5 +14,59 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("handle hello")
-	fmt.Fprintf(w,"hello")
+	fmt.Fprintf(w, "hello")
+}
+
+const form = `<html>
+<body>
+<form action="#" method="post" name="bar">
+<input type="text" name="in"/>
+<input type="text" name="in"/>
+<input type="submit" value="Submit"/>
+</form>
+<body>
+</html>`
+
+func SimpleServer(w http.ResponseWriter, request *http.Request) {
+	io.WriteString(w, "<h1>Hello World</h1>")
+	panic("test test")
+}
+
+func FormServer(w http.ResponseWriter, request *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	switch request.Method {
+	case "GET":
+		io.WriteString(w, form)
+	case "POST":
+		request.ParseForm()
+		io.WriteString(w, request.Form["in"][0])
+		io.WriteString(w, "\n")
+		io.WriteString(w, request.FormValue("in"))
+	}
+}
+
+/**
+处理Panic错误
+*/
+func logPanics(handle func(w http.ResponseWriter, r *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if x := recover(); x != nil {
+				log.Printf("[%v] caugh panic: %v", request.RemoteAddr, x)
+			}
+		}()
+		handle(writer, request)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", logPanics(Hello))
+	http.HandleFunc("/user/login", logPanics(Login))
+	http.HandleFunc("/test1", logPanics(SimpleServer))
+	http.HandleFunc("/test2", logPanics(FormServer))
+	err := http.ListenAndServe("0.0.0.0:8880", nil)
+	if err != nil {
+		fmt.Println("http_server listen failed,err=", err)
+	}
+
 }
---
+++ GolangStudy/go_study_20190607/template/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+)
+
+type Person struct {
+	Name string
+	age string
+}
+
+func main() {
+	t,err:=template.ParseFiles("D:/index.html")
+	if err!=nil{
+		fmt.Println("parse file err: ",err)
+		return
+	}
+	p:=Person{Name:"Mary",age:"31"}
+	if err := t.Execute(os.Stdin, p);err!=nil {
+		fmt.Println("There was an error",err.Error())
+	}
+}
